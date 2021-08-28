5614b39499de3f64bcf8c3bc7ef2b3bab204ce0d
joancastillo <joan.castillo2507@gmail.com>
2021-08-28 13:20:31 -0400

    proyecto terminado

---
+++ app.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+    let whoRandom = Math.floor(Math.random() * who.length); 
+    let actionRandom = Math.floor(Math.random() * action.length);
+    let whatRandom = Math.floor(Math.random() * what.length);
+    let whenRandom = Math.floor(Math.random() * when.length);
+    
+    document.querySelector('#excuse').innerHTML = `${who[whoRandom]} 
+    ${action[actionRandom]} ${what[whatRandom]} ${when[whenRandom]}` ;
+}
